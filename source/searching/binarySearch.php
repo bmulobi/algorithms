2e55f4625c4ba7edfd3456a3958b674421d432a3
Bernard Mulobi <bmulobi@gmail.com>
2018-08-20 02:03:49 +0300

    initial
    Signed-off-by: Bernard Mulobi <bmulobi@gmail.com>

---
+++ source/searching/binarySearch.php
@@ -0,0 +1,34 @@
+<?php
+    /**
+     * @param array $input  search space
+     * @param mixed $needle what we're searching for
+     *
+     * @return int|string
+     */
+    function binarySearch(array $input, $needle) {
+        $length = count($input);
+
+        if (!$length) {
+            return "Input is empty";
+        } elseif($length === 1) {
+            return $needle == $input[0] ? 0 : -1;
+        }
+
+        $low = 0;
+        $high = $length - 1;
+
+
+        while ($low <= $high) {
+            $midPosition = floor(($high + $low) / 2);
+
+            if ($needle == $input[$midPosition]) {
+                return $midPosition;
+            } elseif ($needle < $input[$midPosition]) {
+                $high = $midPosition - 1;
+            } else {
+                $low = $midPosition + 1;
+            }
+        }
+
+        return -1;
+    }
---
+++ source/searching/linearSearch.php
@@ -0,0 +1,19 @@
+<?php
+
+    /**
+     * @param array $input  search space
+     * @param mixed $needle what we're searching for
+     *
+     * @return int|string
+     */
+    function linearSearch(array $input, $needle) {
+        if (!$input) { return "Input is empty"; }
+
+        foreach ($input as $key => $value) {
+            if ($needle == $value) {
+                return $key;
+            }
+        }
+
+        return -1;
+    }
