69dbfbbbdee8c6714e4c1e2efa1c9afec2bc10e1
Bernard Mulobi <bmulobi@gmail.com>
2018-09-20 04:48:09 +0300

    Add heaps
    Signed-off-by: Bernard Mulobi <bmulobi@gmail.com>

---
+++ Source/Datastructures/Heaps/MaxHeap.php
@@ -0,0 +1,29 @@
+<?php
+
+class MaxHeap Implements Heap
+{
+    public function __construct()
+    {
+
+    }
+
+    public function constructHeap(array $data)
+    {
+        // TODO: Implement constructHeap() method.
+    }
+
+    public function insert($data)
+    {
+        // TODO: Implement insert() method.
+    }
+
+    public function delete()
+    {
+        // TODO: Implement delete() method.
+    }
+
+    public function pop()
+    {
+        // TODO: Implement pop() method.
+    }
+}
