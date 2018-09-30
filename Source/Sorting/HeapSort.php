69f75920f396db78f457d9e0cfe97a917a117226
Bernard Mulobi <bmulobi@gmail.com>
2018-10-01 01:07:18 +0300

    Refactor heapsort again
    Signed-off-by: Bernard Mulobi <bmulobi@gmail.com>

--- Source/Sorting/HeapSort.php
+++ Source/Sorting/HeapSort.php
@@ -2,31 +2,34 @@
 require_once "Source/DataStructures/Heaps/MaxHeap.php";
 require_once "Source/DataStructures/Heaps/MinHeap.php";
 
-function nonDecreasingHeapSort(array $items) {
-    $result = [];
-    $minHeap = new MinHeap($items);
+const NON_DECREASING = 'NON_DECREASING';
+const NON_INCREASING = 'NON_INCREASING';
 
-    while (!$minHeap->isEmpty()) {
-        $result[] = $minHeap->poll();
+function heapSort(array $items, $flag = NON_DECREASING) {
+    $result = [];
+
+    switch ($flag) {
+        case 'NON_DECREASING':
+            $heap = new MinHeap($items);
+            break;
+        case 'NON_INCREASING':
+            $heap = new MaxHeap($items);
+            break;
+        default:
+            throw new Exception("Unknown flag");
+    }
+
+    while (!$heap->isEmpty()) {
+        $result[] = $heap->poll();
     }
 
     return $result;
 }
 
-function nonIncreasingHeapSort(array $items) {
-    $result = [];
-    $maxHeap = new MaxHeap($items);
-
-    while (!$maxHeap->isEmpty()) {
-        $result[] = $maxHeap->poll();
-    }
-
-    return $result;
-}
 
 
-echo "\nnonDecreasingHeapSort([1,2,7,7,8,3,8,4,100,5,4,6,7,8,9,4,5,-3,5,5,8,7])\n";
-var_dump(nonDecreasingHeapSort([1,2,7,7,8,3,8,4,100,5,4,6,7,8,9,4,5,-3,5,5,8,7]));
+echo "\nnonDecreasingHeapSort\n";
+var_dump(heapSort([1,2,7,7,8,3,8,4,100,5,4,6,7,8,9,4,5,-3,5,5,8,7]));
 
-echo   "\nnonIncreasingHeapSort([1,2,7,7,8,3,8,4,100,5,4,6,7,8,9,4,5,-3,5,5,8,7])\n";
-var_dump(nonIncreasingHeapSort([1,2,7,7,8,3,8,4,100,5,4,6,7,8,9,4,5,-3,5,5,8,7]));
+echo   "\nnonIncreasingHeapSort\n";
+var_dump(heapSort([1,2,7,7,8,3,8,4,100,5,4,6,7,8,9,4,5,-3,5,5,8,7], NON_INCREASING));
